f6a72c15606b404140befc6bbcc88591f610a117
Felipe Marques <fmarques.ds@gmail.com>
2022-08-27 17:21:18 -0300

    Project skeletoon

---
+++ src/main/java/com/fiap/clouddevelopment/apifiapclouddevelopmenttrabalho/ApiFiapCloudDevelopmentTrabalhoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.clouddevelopment.apifiapclouddevelopmenttrabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFiapCloudDevelopmentTrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFiapCloudDevelopmentTrabalhoApplication.class, args);
+	}
+
+}
